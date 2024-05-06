aa5c6239db24d78f7f02a7fc8949550e5a0d3d9d
Dexter <71167911+soham-basak@users.noreply.github.com>
2024-05-06 17:17:28 +0530

    feat: :sparkles: next-auth setup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from 'drizzle-kit'
+import dotenv from 'dotenv'
+
+dotenv.config({
+    path: './.env',
+})
+
+const DATABASE_URL = process.env.DATABASE_URL;
+
+if (!DATABASE_URL) {
+  throw new Error('Database URL not specified!');
+}
+
 export default defineConfig({
  schema: "./src/db/schema.ts",
+ out: './migrations',
   driver: 'pg',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    connectionString: DATABASE_URL,
   },
   verbose: true,
   strict: true,
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { ThemeProvider } from "@/components/theme-provider";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ModeToggle } from "@/components/mode-toggle";
+import { Providers } from "./provider";
+import { Header } from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,18 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {" "}
-          <div>
-            <ModeToggle />
-          </div>
+        <Providers>
+          <Header />
           {children}
-        </ThemeProvider>
+        </Providers>
       </body>
     </html>
   );
