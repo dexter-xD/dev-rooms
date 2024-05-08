65735a04450319fe274913e6008f1229aca16ad6
Dexter <71167911+soham-basak@users.noreply.github.com>
2024-05-08 15:32:36 +0530

    feat: Displaying the rooms on the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,24 @@
+import RoomCard from "@/components/room-card";
 import { Button } from "@/components/ui/button";
-import { db } from "@/db";
+import { getRooms } from "@/services/rooms";
 import Link from "next/link";
 
 export default async function Home() {
-  const rooms = await db.query.room.findMany();
+  const rooms = await getRooms();
 
   return (
     <main className="min-h-screen container mx-auto py-16">
-      <div className="flex justify-between items-center mx-">
+      <div className="flex justify-between items-center mb-8">
         <h1 className="text-4xl font-bold">Find Dev Rooms</h1>
         <Button asChild>
           <Link href="/create-room">Create Room</Link>
         </Button>
       </div>
-      {rooms.map((room) => {
-        return (
-          <div key={room.id}>
-            <h1>{room.name}</h1>
-            <h3>{room.description}</h3>
-          </div>
-        );
-      })}
+      <div className="grid grid-cols-3 gap-4">
+        {rooms.map((room) => {
+          return <RoomCard room={room} key={room.id} />;
+        })}
+      </div>
     </main>
   );
 }
---
+++ src/components/room-card.tsx
@@ -0,0 +1,40 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Room } from "@/db/schema";
+import { GithubIcon } from "lucide-react";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+export default function RoomCard({ room }: { room: Room }) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{room.name}</CardTitle>
+        <CardDescription>{room.description}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        {room.githubrepo && (
+          <Link
+            href={room.githubrepo}
+            className="flex items-center gap-2"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GithubIcon /> GitHub Repository
+          </Link>
+        )}
+      </CardContent>
+      <CardFooter>
+        <Button asChild>
+          <Link href={`rooms/{room.id}`}>Join Room</Link>
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
