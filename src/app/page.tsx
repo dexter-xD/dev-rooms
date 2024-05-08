941ff392579d1f23715905d3851f49d32f805bb1
Dexter <71167911+soham-basak@users.noreply.github.com>
2024-05-08 14:50:26 +0530

    feat: TopLoader and Landing page styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
+import { Button } from "@/components/ui/button";
 import { db } from "@/db";
+import Link from "next/link";
 
 export default async function Home() {
   const rooms = await db.query.room.findMany();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="min-h-screen container mx-auto py-16">
+      <div className="flex justify-between items-center mx-">
+        <h1 className="text-4xl font-bold">Find Dev Rooms</h1>
+        <Button asChild>
+          <Link href="/create-room">Create Room</Link>
+        </Button>
+      </div>
       {rooms.map((room) => {
         return (
           <div key={room.id}>
